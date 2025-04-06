e7dbdcc3502328441b674a244b7efc3f8a77053a
Gabriel Natan <gabriel--natan@live.com>
2025-04-06 16:09:01 -0300

    feat(auth): add JWT support and implement sign-in endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConfig } from './config/jwt.config';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [JwtModule.register(jwtConfig), PrismaModule, UsersModule],
+  imports: [
+    JwtModule.register(jwtConfig),
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign-in.dto';
+import { Request as ExpressRequest } from 'express';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req: ExpressRequest) {
+    return req.user;
+  }
+}
